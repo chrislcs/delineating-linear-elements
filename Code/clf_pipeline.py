b6bc815c30ebfc99e40a82dc70c0a5840e21afe1
Chris <c_lucas111@hotmail.com>
2017-07-13 09:37:01 +0200

    removed some unnecessary code

--- Code/clf_pipeline.py
+++ Code/clf_pipeline.py
@@ -5,8 +5,6 @@
 """
 
 import pandas as pd
-import numpy as np
-import time
 from clf_preprocessing import merge_dataframes, correlated_features
 from clf_classifiers import BalancedRandomForest, classify_vegetation
 from clf_assessment import (grid_search, cross_validation,
@@ -23,10 +21,6 @@ data.rename(columns=lambda x: x.replace(',', '_'), inplace=True)
 del veg_pc, non_veg_pc
 class_cat, class_indexer = pd.factorize(data['class'])
 data['class_cat'] = class_cat
-del class_cat
-class_dtype = '|S%s' % max([len(x) for x in classes])
-del x
-class_indexer = np.array(class_indexer, dtype=class_dtype)
 
 # %% Define the feature space
 features = data.columns.drop(['class', 'class_cat', 'X', 'Y', 'Z',
@@ -72,7 +66,6 @@ for i in xrange(parts):
 point_cloud['class'] = classification
 
 # %% Classify trees / low vegetation
-t = time.time()
 points = point_cloud.loc[point_cloud['class'] == 1].as_matrix(columns=['X', 'Y', 'Z'])
 radius = 2.0
 tree_th = 4.0
@@ -80,7 +73,6 @@ classification = classify_vegetation(points, radius, tree_th)
 point_cloud['veg_class'] = 'non_veg'
 point_cloud.loc[point_cloud['class'] == 1, 'veg_class'] = classification
 point_cloud['class'], _ = pd.factorize(point_cloud['veg_class'])
-print "Done! Runtime: %s" % str(time.time() - t)
 
 # %% Save results
 point_cloud.to_csv('../Data/veg_classification.csv',
